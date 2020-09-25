c27f2b4f853b03c5a2678125b4908e7164fd9d3b
moofoo <nathancookdev@gmail.com>
2020-09-25 13:39:37 -0500

    disabled sourcemaps, reducing bundle size significantly

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,12 @@ import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
 
 import pkg from './package.json';
+
+const env = process.env.NODE_ENV;
+
 export default {
   input: 'src/index.tsx',
   output: [
@@ -14,9 +18,13 @@ export default {
       format: 'es',
       exports: 'named',
       name: 'rjr-core',
-      sourcemap: true,
+      sourcemap: false,
       strict: false,
       plugins: [terser()],
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      },
     },
   ],
   plugins: [
@@ -25,6 +33,7 @@ export default {
       main: true,
       browser: true,
     }),
+    replace({ 'process.env.NODE_ENV': JSON.stringify('env') }),
     commonjs(),
     typescript(),
     babel({
